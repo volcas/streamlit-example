8d93b6703988281835580538da46473d28dfb491
volcas <nnair25@gmail.com>
2023-03-08 18:06:10 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -26,7 +26,9 @@ data2 = st.file_uploader('Upload second file here',type='csv')
 
 
 
-def model_run(appdata_main,select_region,select_team1,select_team2):
+def model_run(appdata,select_region,select_team1,select_team2):
+    select_region= st.sidebar.selectbox('Select Region',
+                                    region_list)
     region=select_region
     appdata=appdata[appdata['Region']==region]
 #         appdata=appdata[(appdata['matchName'].str.contains(select_team1)) & (appdata['matchName'].str.contains(select_team2))]    
