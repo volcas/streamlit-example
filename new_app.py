f731da9d4f32e01b84cbcf9988e9db3d5267d943
volcas <nnair25@gmail.com>
2023-03-08 18:07:29 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -27,9 +27,9 @@ data2 = st.file_uploader('Upload second file here',type='csv')
 
 
 def model_run(appdata,select_region,select_team1,select_team2):
-    select_region2= st.sidebar.selectbox('Select Region',
+    select_region= st.sidebar.selectbox('Select Region',
                                     region_list)
-    region=select_region2
+    region=select_region
     appdata=appdata[appdata['Region']==region]
 #         appdata=appdata[(appdata['matchName'].str.contains(select_team1)) & (appdata['matchName'].str.contains(select_team2))]    
     df_cat = pd.concat([pd.DataFrame(typeOfDay_cat_encoder.transform(appdata[['typeOfDay']]), columns=typeOfDay_cat_encoder.get_feature_names_out(),
@@ -125,8 +125,8 @@ if data is not None and data2 is not None:
            'MP/Chhattisgarh', 'Mah / Goa', 'Odisha', 'Pun/Cha', 'Rajasthan',
            'TN/Pondicherry', 'UP/Uttarakhand', 'West Bengal']
 
-    select_region= st.sidebar.selectbox('Select Region',
-                                    region_list)
+#     select_region= st.sidebar.selectbox('Select Region',
+#                                     region_list)
 
 
 
