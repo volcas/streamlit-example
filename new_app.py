d684f2efaefa9dde08736d86610df3bd043514ef
volcas <nnair25@gmail.com>
2023-03-08 14:52:29 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -181,7 +181,7 @@ if data is not None and data2 is not None:
     
     total_pred,total=model_run(appdata,select_region,select_team1,select_team2)
      
-    new_new=appdata[appdata['Region']=select_region]
+    new_new=appdata[appdata['Region']==select_region]
     new_new['pred']=total_pred
     new_new['new_total']=total
 
