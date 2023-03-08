d0e60ce7c5b773dd1e39ba742dd9374d2ae716ad
volcas <nnair25@gmail.com>
2023-03-08 14:26:47 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -47,10 +47,11 @@ if data is not None and data2 is not None:
     with open('model_'+select_tg+'.pkl', 'rb') as f:
         svr = pickle.load(f)
 
-    team_list=['CSK', 'MI', 'RCB', 'LSG', 'RR', 'KKR', 'PBKS', 'GT', 'DC', 'SRH']    
+    team_list1=['CSK', 'MI', 'RCB', 'LSG', 'RR', 'KKR', 'PBKS', 'GT', 'DC', 'SRH']    
     select_team1 = st.sidebar.selectbox('Select Team 1',
                                     team_list)    
-    team_list.remove(select_team1)
+    team_list2=['CSK', 'MI', 'RCB', 'LSG', 'RR', 'KKR', 'PBKS', 'GT', 'DC', 'SRH']    
+    team_list2.remove(select_team1)
     select_team2 = st.sidebar.selectbox('Select Team 2',
                                     team_list)   
 
@@ -74,134 +75,134 @@ if data is not None and data2 is not None:
 
 
 
-
+    while True:
 # if data is not None:
-    col=select_tg
-#     offset = df[df['Datetime']<'2022-05-21'].shape[0]   ## for train test split
-    
-    appdata=appdata_main.copy()
-    typeOfDay_cat = appdata[['typeOfDay']]
+        col=select_tg
+    #     offset = df[df['Datetime']<'2022-05-21'].shape[0]   ## for train test split
 
-    typeOfDay_cat_encoder = OneHotEncoder(sparse=False)
-    typeOfDay_cat_1hot = typeOfDay_cat_encoder.fit_transform(typeOfDay_cat)
+        appdata=appdata_main.copy()
+        typeOfDay_cat = appdata[['typeOfDay']]
 
-    Festival_cat = appdata[['Festival']]
+        typeOfDay_cat_encoder = OneHotEncoder(sparse=False)
+        typeOfDay_cat_1hot = typeOfDay_cat_encoder.fit_transform(typeOfDay_cat)
 
-    Festival_cat_encoder = OneHotEncoder(sparse=False)
-    Festival_cat_1hot = Festival_cat_encoder.fit_transform(Festival_cat)
+        Festival_cat = appdata[['Festival']]
 
-    inning_cat = appdata[['inning']]
+        Festival_cat_encoder = OneHotEncoder(sparse=False)
+        Festival_cat_1hot = Festival_cat_encoder.fit_transform(Festival_cat)
 
-    inning_cat_encoder = OneHotEncoder(sparse=False)
-    inning_cat_1hot = inning_cat_encoder.fit_transform(inning_cat)
+        inning_cat = appdata[['inning']]
 
-    region_cat = appdata[['Region']]
+        inning_cat_encoder = OneHotEncoder(sparse=False)
+        inning_cat_1hot = inning_cat_encoder.fit_transform(inning_cat)
 
-    region_cat_encoder = OneHotEncoder(sparse=False)
-    region_cat_1hot = region_cat_encoder.fit_transform(region_cat)
+        region_cat = appdata[['Region']]
 
-    timeOfDay_cat = appdata[['timeOfDay']]
+        region_cat_encoder = OneHotEncoder(sparse=False)
+        region_cat_1hot = region_cat_encoder.fit_transform(region_cat)
 
-    timeOfDay_cat_encoder = OneHotEncoder(sparse=False)
-    timeOfDay_cat_1hot = timeOfDay_cat_encoder.fit_transform(timeOfDay_cat)
-    
-    appdata=appdata_main[appdata_main['Region']==select_region]
-    appdata=appdata[(appdata['matchName'].str.contains(select_team1)) & (appdata['matchName'].str.contains(select_team2))]    
-    df_cat = pd.concat([pd.DataFrame(typeOfDay_cat_encoder.transform(appdata[['typeOfDay']]), columns=typeOfDay_cat_encoder.get_feature_names_out(),
-                index = appdata.index),
-                pd.DataFrame(Festival_cat_encoder.transform(appdata[['Festival']]), columns=Festival_cat_encoder.get_feature_names_out(),
-                index = appdata.index), 
-                pd.DataFrame(inning_cat_encoder.transform(appdata[['inning']]), columns=inning_cat_encoder.get_feature_names_out(),
-                index = appdata.index),
-                pd.DataFrame(region_cat_encoder.transform(appdata[['Region']]), columns=region_cat_encoder.get_feature_names_out(),
-                index = appdata.index),
-                pd.DataFrame(timeOfDay_cat_encoder.transform(appdata[['timeOfDay']]), columns=timeOfDay_cat_encoder.get_feature_names_out(),
-                index = appdata.index)], axis=1)
+        timeOfDay_cat = appdata[['timeOfDay']]
 
+        timeOfDay_cat_encoder = OneHotEncoder(sparse=False)
+        timeOfDay_cat_1hot = timeOfDay_cat_encoder.fit_transform(timeOfDay_cat)
 
-    df_num = appdata[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore','Pred',col]]
-    X = pd.concat([df_num.iloc[:,:-1], df_cat], axis=1)
-    y = pd.DataFrame(df_num.loc[:,col])
-    offset=len(X)-periods_input
-    X_train = X.loc[0:offset-1,:]
-    X_test = X.loc[offset:,:]
-
-    y_train = y.loc[0:offset-1,:]
-    y_test = y.loc[offset:,:]
-
-    feature_scaler = StandardScaler()
-    X_train[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']] = feature_scaler.fit_transform(X_train[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']])
-
-    target_scaler = StandardScaler()
-    y_train = target_scaler.fit_transform(y_train)
-
-    X_test[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']] = feature_scaler.transform(X_test[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']])
-    y_test = target_scaler.transform(y_test)
-
-#     svr = SVR()
-
-    # train the model on training data
-#     svr.fit(X_train.fillna(0), y_train)
-
-    # make predictions on test data
-    y_pred = svr.predict(X_test.fillna(0))
-    # y_pred=1.33*y_pred
-    # calculate mean squared error
-#     mse = mean_squared_error(y_test, y_pred)
-    y_pred1=svr.predict(X_train.fillna(0))
-    # mse = mean_squared_error(y_train, y_pred1)
-    # print(mse)
-    # print mean squared error
-
-    total_pred=np.concatenate([y_pred1, y_pred])
-    total=np.concatenate([y_train, y_test])
-#     mse = mean_squared_error(total, total_pred)
-
-#     # print mean squared error
-#     print("Mean squared error: ", mse)
-#     df['Pred_'+col]=target_scaler.inverse_transform(total_pred.reshape(-1, 1))                     
-#     print("For the col ",col," mean squared error: ", mse)
+        appdata=appdata_main[appdata_main['Region']==select_region]
+        appdata=appdata[(appdata['matchName'].str.contains(select_team1)) & (appdata['matchName'].str.contains(select_team2))]    
+        df_cat = pd.concat([pd.DataFrame(typeOfDay_cat_encoder.transform(appdata[['typeOfDay']]), columns=typeOfDay_cat_encoder.get_feature_names_out(),
+                    index = appdata.index),
+                    pd.DataFrame(Festival_cat_encoder.transform(appdata[['Festival']]), columns=Festival_cat_encoder.get_feature_names_out(),
+                    index = appdata.index), 
+                    pd.DataFrame(inning_cat_encoder.transform(appdata[['inning']]), columns=inning_cat_encoder.get_feature_names_out(),
+                    index = appdata.index),
+                    pd.DataFrame(region_cat_encoder.transform(appdata[['Region']]), columns=region_cat_encoder.get_feature_names_out(),
+                    index = appdata.index),
+                    pd.DataFrame(timeOfDay_cat_encoder.transform(appdata[['timeOfDay']]), columns=timeOfDay_cat_encoder.get_feature_names_out(),
+                    index = appdata.index)], axis=1)
 
 
-#     forecast = fcst[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+        df_num = appdata[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore','Pred',col]]
+        X = pd.concat([df_num.iloc[:,:-1], df_cat], axis=1)
+        y = pd.DataFrame(df_num.loc[:,col])
+        offset=len(X)-periods_input
+        X_train = X.loc[0:offset-1,:]
+        X_test = X.loc[offset:,:]
 
-#     forecast_filtered =  forecast[forecast['ds'] > max_date]    
-    st.write(total_pred)
+        y_train = y.loc[0:offset-1,:]
+        y_test = y.loc[offset:,:]
 
-    
-    st.write("The next visual shows the actual (red line) and predicted (blue line) values over time.")    
+        feature_scaler = StandardScaler()
+        X_train[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']] = feature_scaler.fit_transform(X_train[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']])
 
-    appdata['pred']=target_scaler.inverse_transform(total_pred.reshape(-1, 1))
-    appdata['new_total']=target_scaler.inverse_transform(total.reshape(-1, 1))
+        target_scaler = StandardScaler()
+        y_train = target_scaler.fit_transform(y_train)
 
-    figure1 =px.line(
-        data_frame =appdata,
-                x = appdata['Datetime'],
-            #     x = test_set['Datetime'].astype(str),
-            #         y=["rat%_Universe scaled", "rr_reqRR_ratio", "maxSR scaled"],
-                y=["new_total","pred"],
-            color_discrete_sequence=['red', "blue"])
-            #     symbol=sample_df['timeOfDay'],
-            #     symbol_sequence=['circle-open', 'square'],
-            #         y = 'rat%_Universe',
-            #     opacity = 0.9,
-            #     orientation = "v",
-            #     barmode = 'group',
-            #     text="match_name"
+        X_test[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']] = feature_scaler.transform(X_test[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']])
+        y_test = target_scaler.transform(y_test)
+
+    #     svr = SVR()
+
+        # train the model on training data
+    #     svr.fit(X_train.fillna(0), y_train)
+
+        # make predictions on test data
+        y_pred = svr.predict(X_test.fillna(0))
+        # y_pred=1.33*y_pred
+        # calculate mean squared error
+    #     mse = mean_squared_error(y_test, y_pred)
+        y_pred1=svr.predict(X_train.fillna(0))
+        # mse = mean_squared_error(y_train, y_pred1)
+        # print(mse)
+        # print mean squared error
+
+        total_pred=np.concatenate([y_pred1, y_pred])
+        total=np.concatenate([y_train, y_test])
+    #     mse = mean_squared_error(total, total_pred)
+
+    #     # print mean squared error
+    #     print("Mean squared error: ", mse)
+    #     df['Pred_'+col]=target_scaler.inverse_transform(total_pred.reshape(-1, 1))                     
+    #     print("For the col ",col," mean squared error: ", mse)
 
 
-            # fig2.update_traces(textposition=sample_df['textPosition'])
+    #     forecast = fcst[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
 
-            #     fig2.add_scatter(x=sample_df['Start Time'], y=sample_df['RR scaled'], name="run rate")
+    #     forecast_filtered =  forecast[forecast['ds'] > max_date]    
+        st.write(total_pred)
 
-            #     fig2.add_trace(go.Table(cells={"values":df.T.values}, header={"values":df.columns}), row=1,col=1)
+
+        st.write("The next visual shows the actual (red line) and predicted (blue line) values over time.")    
+
+        appdata['pred']=target_scaler.inverse_transform(total_pred.reshape(-1, 1))
+        appdata['new_total']=target_scaler.inverse_transform(total.reshape(-1, 1))
+
+        figure1 =px.line(
+            data_frame =appdata,
+                    x = appdata['Datetime'],
+                #     x = test_set['Datetime'].astype(str),
+                #         y=["rat%_Universe scaled", "rr_reqRR_ratio", "maxSR scaled"],
+                    y=["new_total","pred"],
+                color_discrete_sequence=['red', "blue"])
+                #     symbol=sample_df['timeOfDay'],
+                #     symbol_sequence=['circle-open', 'square'],
+                #         y = 'rat%_Universe',
+                #     opacity = 0.9,
+                #     orientation = "v",
+                #     barmode = 'group',
+                #     text="match_name"
 
 
-            # fig2.update_xaxes(tickangle=290)
-    figure1.update_layout(showlegend=True,font=dict(family="Courier New",size=12,color='Black'),
-                                   title=f"SVR with linear kernel model prediction",
-                                   xaxis_title="Time of day",
-                                   yaxis_title="Predicted Viewership",
-                                   width=1000,height=500)
+                # fig2.update_traces(textposition=sample_df['textPosition'])
 
-    st.write(figure1)
+                #     fig2.add_scatter(x=sample_df['Start Time'], y=sample_df['RR scaled'], name="run rate")
+
+                #     fig2.add_trace(go.Table(cells={"values":df.T.values}, header={"values":df.columns}), row=1,col=1)
+
+
+                # fig2.update_xaxes(tickangle=290)
+        figure1.update_layout(showlegend=True,font=dict(family="Courier New",size=12,color='Black'),
+                                       title=f"SVR with linear kernel model prediction",
+                                       xaxis_title="Time of day",
+                                       yaxis_title="Predicted Viewership",
+                                       width=1000,height=500)
+
+        st.write(figure1)
