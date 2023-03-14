f1e9af6eb6eddeab28cd53517cfaed6ef00bfcb2
volcas <nnair25@gmail.com>
2023-03-14 10:39:16 +0530

    Update new_app6.py

--- new_app6.py
+++ new_app6.py
@@ -29,7 +29,7 @@ st.title('Time Series Forecasting Using Streamlit')
 from google.oauth2 import service_account
 from gsheetsdb import connect
 
-Create a connection object.
+
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=[
