3e7209baced4a5e7995c9e69661d45a4843405b3
volcas <nnair25@gmail.com>
2023-04-03 18:08:11 +0530

    Update new_app5.py

--- new_app5.py
+++ new_app5.py
@@ -164,9 +164,10 @@ select_time = st.sidebar.selectbox('Select Time',
 # Combining actual with predictions Model 1
 
 actual_data['Datetime']=pd.to_datetime(actual_data['Time'])
+st.write(type(model1_data['Date']))
 
 model1_data['Date']=model1_data['Date'].astype(str)
-st.write(model1_data['Date'])
+st.write(type(model1_data['Date']))
 
 model1_data['Time']=pd.to_datetime(model1_data['Time']).dt.time
 model1_data['Time']=model1_data['Time'].astype(str)
